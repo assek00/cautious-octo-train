abf0160b2c26a3f2a1e4117f48bab7df686b510b
ahatbabaev-PEPE <ahatbabaev1337@gmail.com>
2026-01-14 10:18:18 +0500

    Update dashboard.js

--- Web Development competiton/dashboard.js
+++ Web Development competiton/dashboard.js
@@ -1,12 +1,1 @@
-// Get issues from localStorage or create empty array
-const issues = JSON.parse(localStorage.getItem("issues")) || [];
 
-// Count total issues
-const totalIssues = issues.length;
-
-// Count resolved issues
-const resolvedIssues = issues.filter(issue => issue.status === "Resolved").length;
-
-// Display values
-document.getElementById("totalIssues").textContent = totalIssues;
-document.getElementById("resolvedIssues").textContent = resolvedIssues;
